b50a84cb31a8e092d1a5655b5eb0ea100fa14f47
yeeeeenai123 <3080636786@qq.com>
2026-07-29 23:59:50 +0800

    Fix compilation errors in MainActivity.kt

--- app/src/main/java/com/smartledger/app/MainActivity.kt
+++ app/src/main/java/com/smartledger/app/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.smartledger.app.ui.screens.HomeScreen
 import com.smartledger.app.ui.screens.StatsScreen
 import com.smartledger.app.ui.theme.*
